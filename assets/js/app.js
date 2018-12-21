e2075cfa619564ace55362354cf8cbcc5bacbebc
Fernando Faria <fernandoafaria@yahoo.com>
2018-12-21 11:48:12 -0500

    about section complete

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let navbar = document.getElementById('navbar');
+    function navChange(){
+        if(window.scrollY > 25){
+            navbar.style.background = 'white';
+            navbar.style.color = "black";
+        }
+        if (window.scrollY < 25) {
+            navbar.style.background = 'transparent';
+            navbar.style.color = "white";
+        }
+    }
+
+
+
+
+
+    document.addEventListener('scroll', navChange);
+    ScrollReveal().reveal('.about-info', {
+        delay: 400,
+        distance: '600px',
+        origin: 'left'
+
+    });
+
+
+
+
+
+})
